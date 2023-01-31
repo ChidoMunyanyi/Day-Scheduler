8e0f264c9d61f7549f202b473367e6065af85c82
ChidoMunyanyi <119809704+ChidoMunyanyi@users.noreply.github.com>
2023-01-31 21:44:59 +0000

    Added local storage for timeblocks

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ $(".saveBtn").on("click", function() {
 })
 
 // Get Key and Text value from local storage and render to page on page load//
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14.description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
