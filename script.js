9c5ecca02e8ce6159224ffbce5b5bc34ecdbfec1
ChidoMunyanyi <119809704+ChidoMunyanyi@users.noreply.github.com>
2023-01-31 21:34:01 +0000

    Added current date and click function on save button

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-var timeBlocks = 
+var currentDay = moment().format('MMMM Do YYYY')
+$("#currentDay").text(currentDay)
+// Add Click Event to save button that grabs value of text area and saves to local storage //
+$(".saveBtn").on("click", function() {
+ var blockKey = $(this).parent().attr("id")
+ var textValue = $(this).siblings("textarea").val()
+    localStorage.setItem(blockKey, textValue)
+})
+
+// Get Key and Text value from local storage and render to page on page load//
